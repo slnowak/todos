75c9906ce8487d5f29946dedcd5d6a085ba57b7c
novy154 <slnowak154@gmail.com>
2014-11-03 07:01:22 +0100

    Integration with Flyway.

---
+++ src/main/resources/db/migration/V1__base_version.sql
@@ -0,0 +1,8 @@
+CREATE TABLE Todo
+(
+    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    description VARCHAR(255),
+    name VARCHAR(255),
+    priority INT,
+    status INT
+);
---
+++ src/main/resources/db/migration/V2__data_insertion.sql
@@ -0,0 +1,5 @@
+insert into todos.Todo (id, name, description, priority, status) values
+  (1, 'todo1', 'fancy todo', 0, 0),
+  (2, 'todo2', 'fancy todo', 2, 1),
+  (3, 'todo3', 'todo', 2, 0),
+  (4, 'todo4', 'to fancy do', 2, 0)
